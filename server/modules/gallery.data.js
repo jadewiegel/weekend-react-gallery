2e94f227310f413651202f207bf602ce1728083f
Jade Wiegel <jadewiegel@gmail.com>
2023-01-19 10:06:07 -0600

    add photos and file paths to project.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/lady.jpg', description: 'Photo of Lady looking cute begging for food.', likes: 0 },
+    { id: 3, path: 'images/pups.jpg', description: 'Axel and Lady waiting for treats.', likes: 0 },
+    { id: 4, path: 'images/sleepy_pups.jpg', description: 'Axel and Lady cuddling taking a snooze.', likes: 0 },
+    { id: 5, path: 'images/nats_photo.jpg', description: 'Talking theory between runs at nationals.', likes: 0 },
+    { id: 6, path: 'images/action_photo.jpg', description: 'Someone else driving my car on course.', likes: 0 },
+    { id: 7, path: 'images/tow_bikes.jpg', description: 'Taking the bikes to storage for winter.', likes: 0 },
 ];
 
 module.exports = galleryItems;
